67af79d2e17574bd5450543ca1bc6fcc4abbb76d
Andrii <blayson4@gmail.com>
2020-11-02 19:01:38 +0100

    change version

--- scraper.js
+++ scraper.js
@@ -97,7 +97,7 @@ export const scrape = async (start, end) => {
     }
 
     await browser.close();
-    
+
     console.log('Scraped ' + counter + ' records, from ' + start + ' to ' + end)
     console.log('Skipped ' + skip_counter + ' records')
     console.log('Saving data...')
